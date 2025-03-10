8b930d7422e686b93b82efa4598d85055ded860b
Gustavo Leal <GustavoLealP@outlook.com>
2025-03-10 13:36:22 -0300

    Criado primeira parte

---
+++ LaGrife/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaGrife.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
