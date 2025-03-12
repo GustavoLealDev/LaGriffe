2dcd612a229d84ceeceb3ece8111dffcad33725a
Gustavo Leal <GustavoLealP@outlook.com>
2025-03-12 20:49:59 -0300

    Criando cadastro de Lojas

---
+++ LaGrife/Controllers/LojasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LaGrife.Models;
+
+namespace LaGrife.Controllers
+{
+    public class LojasController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Loja> list = new List<Loja>();
+            list.Add(new Loja { Id = 1, Local = "Av. Dr. Eugênio Borges, 1737 - Arsenal "});
+            return View(list);
+        }
+    }
+}
---
+++ LaGrife/Models/Loja.cs
@@ -0,0 +1,8 @@
+﻿namespace LaGrife.Models
+{
+    public class Loja
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+    }
+}
