76571773a1919d65f922e58498275426d60015cd
Gustavo Leal <GustavoLealP@outlook.com>
2025-03-17 01:36:04 -0300

    Adicionando Controller e Index de Vendedores

---
+++ LaGrife/Controllers/VendedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LaGrife.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
